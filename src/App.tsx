9fa67d87d2082717cb7eb443f66b5510e0b7182d
Ricky-Santiago <santiagoricky2004@gmail.com>
2026-02-05 11:49:27 -0500

    Initial commit: Portfolio setup with React + TypeScript
    - Created project with Vite + React + TypeScript
    - Organized folder structure for portfolio
    - Installed React Router DOM
    - Added basic layout and pages structure

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,1 @@
+export default function Header() { return <header>Temporal Header</header> }
